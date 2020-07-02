bcfc66d724579a366a194c4aceb3bd2640078f46
bhurst8 <67595898+bhurst8@users.noreply.github.com>
2020-07-02 14:06:47 -0500

    first commit

---
+++ app/index.js
@@ -0,0 +1,34 @@
+/*
+ * Entry point for the watch app
+ */
+
+console.log("App code started");
+
+import document from "document";
+import { Barometer } from "barometer";
+
+// Fetch UI elements we will need to change
+var altitudeLabel = document.getElementById("altitude");
+var pressureLabel = document.getElementById("pressure");
+
+// Initialize the UI with some values
+altitudeLabel.text = "--";
+pressureLabel.text = "--";
+
+// Create a new instance of the Barometer
+var bar = new Barometer();
+
+// Update the lavel with each reading from the sensor
+bar.onreading = () => {
+  altitudeLabel.text = altitudeFromPressure(bar.pressure / 100) + " ft";
+  pressureLabel.text = Math.round(bar.pressure / 100) + " hPa";
+}
+
+// Begin monitoring the sensor
+bar.start();
+
+// Converts pressure in millibars to altitude in feet
+// https://en.wikipedia.org/wiki/Pressure_altitude
+function altitudeFromPressure(pressure) {
+  return (1 - (pressure/1013.25)**0.190284)*145366.45;
+}
